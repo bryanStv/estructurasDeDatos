cf6990ae7c2f56ebc2ee54a5e2777898d9b684a6
bryanStv <bryans.oyola@gmail.com>
2023-12-04 10:08:32 +0100

    7

---
+++ src/AddAll.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AddAll {
+    public static void main(String[] args) {
+        List<Integer> lista1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+        ArrayList<Integer> lista2 = new ArrayList<>();
+        lista2.addAll(lista1);
+        System.out.println(lista2.toString());
+    }
+}
