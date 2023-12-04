ea3ef521666108f008533592697cb05ee4dcc7e6
bryanStv <bryans.oyola@gmail.com>
2023-12-04 09:00:09 +0100

    first commit

---
+++ src/Coches.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Coches {
+    public static void main(String[] args) {
+        ArrayList<String> coches = new ArrayList<>();
+        coches.add("Volvo");
+        coches.add("Seat");
+        coches.add("Fiat");
+        for (int i = 0; i < coches.size(); i++) {
+            System.out.println(coches.get(i));
+        }
+        for(String coche : coches){
+            System.out.println(coche);
+        }
+    }
+}
