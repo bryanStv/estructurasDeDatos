9b80873ee10557033faf91a12ba01824820d7450
bryanStv <bryans.oyola@gmail.com>
2023-12-11 08:49:43 +0100

    v2.1

--- src/Telefonos2.java
+++ src/Telefonos2.java
@@ -1,18 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Telefonos2 {
     public static void main(String[] args) {
-        Map<String, Integer> telefonos = new HashMap<>();
-        telefonos.put("Pepito",123456789);
-        telefonos.put("Jaimito",987654321);
-        telefonos.put("Empresaurio",123789456);
+        Map<String, List<Integer>> telefonos = new HashMap<>();
+        telefonos.put("Pepito", Arrays.asList(123456789,991));
+        telefonos.put("Jaimito",Arrays.asList(112,856));
+        telefonos.put("Empresaurio",Arrays.asList(123,321));
 
         Scanner tc = new Scanner(System.in);
         System.out.print("Dime un nombre: ");
         String nombre = tc.next();
-        System.out.println(telefonos.getOrDefault(nombre,-1));
+        System.out.println(telefonos.getOrDefault(nombre,null));
         tc.close();
     }
 }
