bf1e492843f5e6bbb6f601f4d09f3f6234c9851a
bryanStv <bryans.oyola@gmail.com>
2023-12-11 09:09:21 +0100

    v2.2

--- src/Telefonos2.java
+++ src/Telefonos2.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Telefonos2 {
     public static void main(String[] args) {
-        Map<String, List<Integer>> telefonos = new HashMap<>();
-        telefonos.put("Pepito", Arrays.asList(123456789,991));
-        telefonos.put("Jaimito",Arrays.asList(112,856));
-        telefonos.put("Empresaurio",Arrays.asList(123,321));
+        Map<String, List<String>> telefonos = new HashMap<>();
+        telefonos.put("Pepito", Arrays.asList("123456789","991"));
+        telefonos.put("Jaimito",Arrays.asList("112","856"));
+        telefonos.put("Empresaurio",Arrays.asList("123","321"));
 
         Scanner tc = new Scanner(System.in);
         System.out.print("Dime un nombre: ");
---
+++ src/TerminacionLoteria.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TerminacionLoteria {
+    public static void main(String[] args) {
+        List<String> numeros = Arrays.asList("00004","03847","39804");
+        System.out.println(repetidos(numeros));
+    }
+
+    public static Map<Integer,Integer> repetidos(List<String> numeros){
+        Map<Integer,Integer> repetidos = new HashMap<>();
+        for(int i = 0; i < numeros.size();i++){
+
+        }
+        return repetidos;
+    }
+}
