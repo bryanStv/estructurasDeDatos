e643ad0e014ec44d07f22606e12ec0e3ca70537a
bryanStv <bryans.oyola@gmail.com>
2023-12-14 09:36:00 +0100

    ft. Eduard & Adrián 2.0

--- src/Anagramas.java
+++ src/Anagramas.java
@@ -1,16 +1,48 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagramas {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new FileReader("archivos/1.txt"));
-        String line;
-        while ((line = reader.readLine())!=null) {
-            //Trabajar con line
-            System.out.println(line);
+    public static void main(String[] args){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("archivos/1.txt"));
+            String palabra,alfabetica = "";
+            Map<String, ArrayList<String>> anagramas = new HashMap<>();
+            ArrayList<String> lista = new ArrayList<>();
+            while ((palabra = reader.readLine())!=null) { //leyendo palabra
+                alfabetica = alfabetizar(palabra);
+                lista = anagramas.get(alfabetica);
+                if(lista == null){
+                    lista = new ArrayList<>();
+                    lista.add(palabra);
+                    anagramas.put(alfabetica, lista);
+                }else{
+                    lista.add(palabra);
+                    anagramas.put(alfabetica,lista);
+                }
+            }
+            if(lista.isEmpty()){
+                System.out.println("el fichero está vacío");
+            }
+            reader.close();
+            int minGroupSize = 2;
+            for (String claves: anagramas.keySet()) {
+                lista = anagramas.get(claves);
+                if(lista.size() >= minGroupSize) {
+                    System.out.println(claves + "-->" + anagramas.get(claves));
+                }
+            }
+        }catch (IOException e){
+            System.out.println("El fichero no existe.");
         }
-        reader.close();
+    }
+    public static String alfabetizar(String line){
+        char[] c = line.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
