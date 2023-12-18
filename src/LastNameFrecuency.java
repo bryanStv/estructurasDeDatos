3bf8f599aef0fbd7aa463ef54dd92df53142b067
bryanStv <bryans.oyola@gmail.com>
2023-12-18 10:35:30 +0100

    ft. Eduard & Adrián 3.5

--- src/LastNameFrecuency.java
+++ src/LastNameFrecuency.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -9,21 +10,18 @@ public class LastNameFrecuency {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("archivos/LastnameFrequencies.csv"));
             String datos, Napellidos,apellidos, numeros;
-            Integer numero = 0;
 
             String[] tabla;
-            Map<String, Integer> paises = new HashMap<>();
+            Map<String, String> paises = new HashMap<>();
 
             reader.readLine();
             while ((datos = reader.readLine())!=null) { //leyendo palabras
                 tabla = datos.split(",");
-                Napellidos = tabla[1];
-                Napellidos.replaceAll("\\.", "");
-                System.out.println(Napellidos);
-                /*if(numero == null){
-                    paises.put(Napellidos, 1);
-                }else{
-                    paises.put(Napellidos,numero+1);
+                if(tabla.length == 2) {
+                    apellidos = tabla[0];
+                    Napellidos = tabla[1];
+                    Napellidos = Napellidos.replaceAll("\\.", "");
+                    numeros = paises.putIfAbsent(apellidos, Napellidos);
                 }
             }
             if(paises.isEmpty()){
@@ -32,11 +30,8 @@ public class LastNameFrecuency {
 
             reader.close();
             for (String claves: paises.keySet()) {
-                numero = paises.get(claves);
-                System.out.println(claves+" --> "+numero+" alumnos");
-            }
-
-                 */
+                numeros = paises.get(claves);
+                System.out.println(claves+" --> "+numeros);
             }
         }catch (IOException e){
             System.out.println("El fichero no existe.");
